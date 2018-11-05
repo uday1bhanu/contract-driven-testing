42b0e940a6983d9dcb7c241af9b3afa8375681e7
Uday Bhanu Prasad Kanagala <uday.kanagala@anaplan.com>
2018-11-04 20:37:08 -0800

    intital project setup for part-II

---
+++ bar-consumer/src/main/java/com/example/barconsumer/BarConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.barconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarConsumerApplication.class, args);
+	}
+}
---
+++ bar-producer/src/main/java/com/example/barproducer/BarProducerApplication.java
@@ -0,0 +1,12 @@
+package com.example.barproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarProducerApplication.class, args);
+	}
+}
